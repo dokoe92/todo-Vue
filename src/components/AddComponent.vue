9f091bc8637c2fec4ebf0a746aba80f345eadf50
pm-User 04 <pm-User04@edu.bfi.wien>
2023-02-20 20:16:35 +0100

    first commit

---
+++ src/components/AddComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>What needs to be done?</h2>
+        <input type="text">
+        <button>Add</button>
+    </div>
+
+</template>
+
+
+<script setup>
+
+</script>
+
+
+<style scoped>
+
+
+</style>
---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <button>{{ props.buttonName }}</button>
+</template>
+
+
+
+<script setup>
+    const props = defineProps(["buttonName", "class"]);
+
+</script>
+
+<style scoped>
+    .btnAdd {
+
+    }
+
+    . {
+
+    }
+
+    .btnDelete{
+
+    }
+
+
+</style>
